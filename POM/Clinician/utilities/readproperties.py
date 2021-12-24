4fdcd2cd82c12b810005f57d9803a5f5b51eb851
anithagd30 <anithagd30@qxf2.com>
2021-12-23 23:03:40 -0500

    made changes in config file path

--- POM/Clinician/utilities/readproperties.py
+++ POM/Clinician/utilities/readproperties.py
@@ -2,7 +2,7 @@ import configparser
 
 config = configparser.RawConfigParser()
 configFilePath = r'C:\Users\oviya\Software\Python\Lib\__pycache__\configparser.cpython-310.pyc'
-config.read("https://github.com/anithagd30/PageObjectModel/blob/97e6313b5c2592fcc5abbfc12eb6a8965abe9941/POM/Clinician/Configuration/config.ini")
+config.read("POM/Clinician/Configuration/config.ini")
 
 class Readconfig:
     @staticmethod
